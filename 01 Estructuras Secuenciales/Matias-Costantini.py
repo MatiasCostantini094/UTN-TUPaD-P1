51644d50d22095a6bf6d5ed5addd08c6db100a85
MatiasCostantini094 <matiascostantini94@gmail.com>
2025-03-30 19:42:13 -0300

    Se carga el Trabajo Practico nr 1 en repositorio

---
+++ 01 Estructuras Secuenciales/Matias-Costantini.py
@@ -0,0 +1,77 @@
+#Trabajo practico numero 1 - 
+#Nombre Matias Costantini 
+
+#Ej 1-
+
+#print("Hola Mundo!")
+
+#Ej 2  - 
+# nombre= input("Ingrese su nombre")
+#  print(f"Hola{nombre}")  
+
+
+#Ej 3 -
+# nombre =  input("Ingrese su nomre")
+# apellido = input("Ingrese su apellido")
+# edad = int(input("Ingrese su edad"))
+#residencia= input("Indiquee su lugar de residencia")
+
+# print(f"Soy {nombre} +{apellido}, tengo {edad} años y vivo {residencia}") 
+
+#Ej 4-
+# radio=  float(input("Indique el radio del circulo"))
+# pi=  3.1416
+# area  = pi * (radio ** 2)
+# perimetro =  2 * pi * radio
+
+# print(f"El area del circulo  es : {area}")
+# print(f"El prerimetro del circulo es {perimetro}")   
+
+
+# Ej 5 -
+# segundos = int(input("Ingrese los segundos deseados"))
+# hora = segundos/3600
+# print(f"{segundos} segundos es igual a {hora} Horas")
+
+#Ej 6-
+# numero= int(input("Ingrese un numero para obtener su tabla de multiplicar"))
+# print(f"La tabla del numero {numero} es")
+# for  i in range(1,11):
+#     tabla= numero * i
+#     print(f"{numero} x {i} = {tabla}")
+
+#Ej 7-
+# numero1= int(input("Ingrese un numero(No puede ser 0):"))
+# while numero1==0:
+#     numero1= int(input("El numero no puede ser 0. Intente de nuevo"))
+# numero2= int(input("Ingres otro numero(No puede ser 0)"))
+# while numero2==0:
+#     numero2= int(input("El numero no puede ser 0. Intente de nuevo "))
+# suma= numero1+numero2
+# resta=numero1-numero2
+# division=numero1/numero2
+# multiplicacion=numero1*numero2
+
+# print(f"La suma es {suma}")
+# print(f"La resta es {resta}")
+# print(f"La division es {division}")
+# print(f"La multiplicacion es {multiplicacion}")
+ 
+#Ej 8-
+# peso=float(input("Ingrese su peso en kg"))
+# altura=float(input("Ingrese su altura"))
+# imc= peso/altura**2
+
+# print(f"El indice de masa corporal es {imc}")
+
+#Ej 9-
+# celsius=float(input("Ingrese la temperatura en Celcius"))
+# fahrenheit= (celsius *  9/5) + 32
+# print(f"{celsius}c es equivalente a {fahrenheit} F")
+
+#Ej 10-
+# numero1= int(input("Ingrese el primer numero"))
+# numero2= int(input("Ingrese el segundo numero"))
+# numero3= int(input("Ingrese el tercer numero"))
+# promedio= (numero1 + numero2 + numero3 )/3
+# print(f"El promedio de los numeros ingresados es {promedio}")
