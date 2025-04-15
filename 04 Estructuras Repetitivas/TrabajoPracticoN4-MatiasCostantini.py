140dd5e15660950b94ac55478b62b4ba151698f7
MatiasCostantini094 <matiascostantini94@gmail.com>
2025-04-15 19:56:12 -0300

    Se guarda README.MD y Hasta la actividad 2 del tp 4

---
+++ 04 Estructuras Repetitivas/TrabajoPracticoN4-MatiasCostantini.py
@@ -0,0 +1,23 @@
+# Trabajo Practico N4  Estructuras repetitivas
+#Matias Costantini 
+"""
+# Actividad 1
+print("Este programa imprime los valores de 0 a 100 , en orden creciente")
+cont = 0
+while cont <= 100:
+    print(cont)
+    cont += 1
+"""
+
+#Actividad 2
+""""
+numero = input("Ingrese un numero entero: ") 
+contador = 0
+
+for caracter in numero:
+    if caracter.isdigit(): # Se asegura que cuente solo los digitos
+        contador += 1
+
+print(f"EL numero tiene {contador} digitos. ")
+
+"""
