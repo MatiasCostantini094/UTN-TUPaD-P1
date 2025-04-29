d50f4172866a845486ed630146cc01dc9c5b05f5
MatiasCostantini094 <matiascostantini94@gmail.com>
2025-04-28 21:15:41 -0300

    Secarga tp 5.1

---
+++ 5.1 Listas/Listas.py
@@ -0,0 +1,69 @@
+# 1 Actividad 
+"""
+lista1Al100= list(range(4,101,4))   #rango de la lista
+print(lista1Al100)
+"""
+# 2 Actividad 
+"""
+UltimoElemento = ["a", "b", "c", "d", "f"]
+print(UltimoElemento[-2])
+"""
+# 3 Actividad
+"""
+lista_vacia = []
+lista_vacia.append("uno")  #agrefa a la lista "uno"
+lista_vacia.append("dos")  #agrefa a la lista "dos"
+lista_vacia.append("tres") #agrefa a la lista "tres"
+print(lista_vacia)
+"""
+# 4 actividad 
+"""
+animales=["perro","gato","conejo","pez"]
+animales[1]= "loro"             # remplaza gato por loro
+animales[3]= "oso"              # remplaza pez por oso
+print(animales) 
+"""
+
+# 5 Actividad 
+# numeros = [8, 15, 3, 22, 7]
+# numeros.remove(max(numeros))
+# print(numeros)
+#En el siguiente codigo , se define una lista de numeros con los valores asignados
+# max(numero)-Encuentra el maximo valor ; remove elimina lo que solicite
+# entonces => numeros.remove(max(numero)) elimina el numero 22
+# print(numeros) muestra la lista despues de la eliminacin
+
+
+# 6 Actividad 
+"""
+lista_numeros = list(range(10,31,5))
+print(lista_numeros[:2])
+"""
+# 7 Actividad 
+"""
+autos = ["sedan","polo","suran","gol"]
+autos[1] = "audi"                      # Remplaza el valor polo
+autos[2] = "ferrari"                   # Remplaza el valor gol
+print(autos)
+"""
+# 8 Avtividad 
+"""
+dobles = []             #lista vacia    
+dobles.append(5*2)      # Agrega el doble de 5
+dobles.append(10*2)     # Agrega el doble de 10
+dobles.append(15*2)     # Agrega el doble de 15
+print(dobles)           # muestra el resultado
+"""
+# 9 Actividad 
+"""
+compras = [["pan","leche"],["arroz","fideos","salsa"],["agua"]]
+compras[2].append("jugo")       # Agrega jugo a la lista 3
+compras[1][1] = "tallarines"    # Cambia fideos por tallarines 2
+compras[0].remove("pan")        #Elimina el pan de la lista 1
+print(compras)                  #imprime el mensaje
+"""
+# 10 Actividad
+"""
+lista_anidada=[15,True, [25.5,57.9,30.6],False]
+print(lista_anidada)
+"""
